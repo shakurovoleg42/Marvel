6d0c58e15a98b2c561a70c24d6e2a451abd6d08d
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-06-12 22:05:59 +0500

    modified:   src/components/app/App.js 	modified:   src/components/charInfo/CharInfo.js 	modified:   src/components/charList/CharList.js 	new file:   src/components/errorBoundary/ErrorBoundary.js 	modified:   src/services/MarvelService.js

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -34,8 +34,6 @@ class CharList extends Component {
         })
     }
 
-    // Этот метод создан для оптимизации, 
-    // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
         const items =  arr.map((item) => {
             let imgStyle = {'objectFit' : 'cover'};
@@ -46,7 +44,8 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
